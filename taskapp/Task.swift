511c58a35f068fe5cfc094c826fb54e13ac457fd
ta7cy <sevenbridge@me.com>
2017-08-03 00:56:20 +0900

    課題前の状態

---
+++ taskapp/Task.swift
@@ -0,0 +1,33 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by tanahashishinhichi on 2017/08/02.
+//  Copyright © 2017年 ta7cy. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object{
+    
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    /// 日時
+    dynamic var date = NSDate()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    
+}
+
